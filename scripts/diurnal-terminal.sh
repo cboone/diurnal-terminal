11dc6a6a0fd7b69eeb706c31f8fe11b4d7729874
Chris Boone <cboone@users.noreply.github.com>
2020-06-07 18:31:14 -0700

    Rework script to figure out the damn time itself

--- scripts/diurnal-terminal.sh
+++ scripts/diurnal-terminal.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+set -e
 
-if [ -z "$0" ]; then
-    echo "USAGE: $0 [sunrise | sunset]"
-    exit 1
-fi
+while read -r LINE; do declare "$LINE"; done < ~/.config/diurnal-terminal.conf
 
-declare "$(env -i "$(cat ~/.config/diurnal-terminal.conf)")"
-
-if [ "$1" = "sunset" ]; then
+HOUR=$(date '+%H')
+if [ "$HOUR" -ge 12 ]; then
     EVENT=sunset
-    OFFSET=SUNSET_OFFSET
-    THEME=NIGHT_THEME
-elif [ "$1" = "sunrise" ]; then
+    OFFSET=$SUNSET_OFFSET
+    THEME=$NIGHT_THEME
+elif [ "$HOUR" -lt 12 ]; then
     EVENT=sunrise
-    OFFSET=SUNRISE_OFFSET
-    THEME=DAY_THEME
+    OFFSET=$SUNRISE_OFFSET
+    THEME=$DAY_THEME
 fi
+
+echo "Waiting for $EVENT $OFFSET to switch to $THEME."
 
 "$(brew --prefix)"/bin/heliocron --latitude "$LATITUDE" --longitude "$LONGITUDE" wait --event "$EVENT" --offset "$OFFSET"; osascript ~/Library/Application\ Support/com.hypsometry.diurnal-terminal/scripts/switch-terminal-theme.scpt "$THEME"
