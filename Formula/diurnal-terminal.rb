a675f8090d897da754fa2d8f68edc1f48bfbf0f4
Christopher Boone <cboone@users.noreply.github.com>
2026-01-05 09:31:36 -0500

    chore: bump version to 0.3.5

--- Formula/diurnal-terminal.rb
+++ Formula/diurnal-terminal.rb
@@ -3,7 +3,7 @@ class DiurnalTerminal < Formula
   homepage "https://github.com/cboone/diurnal-terminal"
   url "https://github.com/cboone/diurnal-terminal.git", branch: "main"
   head "https://github.com/cboone/diurnal-terminal.git", branch: "main"
-  version "0.3.4"
+  version "0.3.5"
 
   depends_on "heliocron"
 
